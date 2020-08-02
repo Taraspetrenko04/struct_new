50b66cbdf3bac57aaeca1ffd3ba712d0685b61b5
unknown <Taraspetrenko04@gmail.com>
2020-08-02 16:00:55 +0300

    Updates

---
+++ precache-manifest.9a317527447ece9ee6f5d8c177a903ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29229f8c6782048ceef52315fe64f8ff",
+    "url": "/struct_new/index.html"
+  },
+  {
+    "revision": "fd35d1d606331b8dbb80",
+    "url": "/struct_new/static/css/2.c21ae415.chunk.css"
+  },
+  {
+    "revision": "b4d43f94b42c245cb3c7",
+    "url": "/struct_new/static/css/main.35e1d513.chunk.css"
+  },
+  {
+    "revision": "fd35d1d606331b8dbb80",
+    "url": "/struct_new/static/js/2.bb7fd01e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/struct_new/static/js/2.bb7fd01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d43f94b42c245cb3c7",
+    "url": "/struct_new/static/js/main.f08db8e1.chunk.js"
+  },
+  {
+    "revision": "fa5f2268a1f013329e6d",
+    "url": "/struct_new/static/js/runtime-main.97f8c0d7.js"
+  },
+  {
+    "revision": "ac0bace5af824dc645acb488a3e33707",
+    "url": "/struct_new/static/media/1.ac0bace5.png"
+  },
+  {
+    "revision": "6896a8407a88ff0abe85d247bddaaf3f",
+    "url": "/struct_new/static/media/architecture.6896a840.jpg"
+  },
+  {
+    "revision": "6f8f21aedc5d96c20539b1927e7c906f",
+    "url": "/struct_new/static/media/construction.6f8f21ae.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/struct_new/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/struct_new/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/struct_new/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/struct_new/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/struct_new/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "edcf9b8d60c5712ed970b02239db2186",
+    "url": "/struct_new/static/media/renovation.edcf9b8d.jpg"
+  }
+]);
